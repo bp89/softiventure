045c4179e68c0037b87e54966413633c8278b0f7
bp89 <banti.prajapati@gmail.com>
2014-11-16 20:29:00 +0530

    refactoring

--- src/main/java/com/softiventure/controller/LoginController.java
+++ src/main/java/com/softiventure/controller/LoginController.java
@@ -23,102 +23,81 @@ import com.softiventure.service.login.LoginServiceImpl;
 
 @Controller
 public class LoginController {
-	@Autowired
-	LoginServiceImpl loginService;
-	//int count = 0;
-	@RequestMapping("/login")
-	public ModelAndView getLoginForm(@ModelAttribute Users users,HttpServletRequest request,
-			@RequestParam(value = "error", required = false) String error,
-			@RequestParam(value = "logout", required = false) String logout) {
-		
-		Map<String, String> map = new HashMap<String, String>();
-		 Enumeration headerNames = request.getHeaderNames();
-		while (headerNames.hasMoreElements()) {
-			String key = (String) headerNames.nextElement();
-			String value = request.getHeader(key);
-			map.put(key, value);
-		}
-		System.out.println(map);
-		String cookie=map.get("cookie");
-		String clientid = cookie+"/login";
-		 HttpSession session = request.getSession(true);
-		 Integer count = (Integer)session.getAttribute("counter");
-		    if (count == null)
-		      count = new Integer(1);
-		    else
-		      count = new Integer(count.intValue() + 1);
-		    session.setAttribute("counter", count);
-	        System.out.println("SessionTracker");
-		    String[] names = session.getValueNames();
-		    for (int i = 0; i < names.length; i++) {
-		      System.out.println(names[i] + ": " + session.getValue(names[i]));
-		    }
-		String message = "";
-		if (error != null) {
-//			if(count<3){
-//			message = "Incorrect username or password !";
-//			//return new ModelAndView("login", "message", message);
-//		}
-//			else {
-//				message = "You have entered incorrect username or password!";
-//				//String counter=Integer.toString(count);
-//				/*session.setAttribute("counter",counter);*/
-//				/*session.setAttribute("counter",count);*/
-//				request.setAttribute("counter",count);
-//				return new ModelAndView("loginWithCaptcha", "message", message);
-//				//return new ModelAndView("form", "message", message);
-//			}
-//		/*else if (logout != null) {
-//			message = "Logout successful !";
-//			return new ModelAndView("home", "message", message);
-//		}*/
+    @Autowired
+    LoginServiceImpl loginService;
+    //int count = 0;
+    @RequestMapping("/login")
+    public ModelAndView getLoginForm(@ModelAttribute Users users,HttpServletRequest request,
+                                     @RequestParam(value = "error", required = false) String error,
+                                     @RequestParam(value = "logout", required = false) String logout) {
+
+        Map<String, String> map = new HashMap<String, String>();
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+        }
+        System.out.println(map);
+        String cookie=map.get("cookie");
+        HttpSession session = request.getSession(true);
+        Integer count = (Integer)session.getAttribute("counter");
+        if (count == null)
+            count = new Integer(1);
+        else
+            count = new Integer(count.intValue() + 1);
+        session.setAttribute("counter", count);
+        System.out.println("SessionTracker");
+        String[] names = session.getValueNames();
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i] + ": " + session.getValue(names[i]));
+        }
+        String message = "";
+        if (error != null) {
             message = "Incorrect username or password !";
-		}
-		return new ModelAndView("login", "message", message);
-	}
-	
-	
+        }
+        return new ModelAndView("login", "message", message);
+    }
 
-	@RequestMapping("/admin**")
-	public String getAdminProfile(@ModelAttribute Users users,HttpServletRequest request) {
-	//	return "admin";
-		//HttpSession session = request.getSession(true);
-		String code = users.getCaptchaCode();
-		String username = users.getUsername();
-		System.out.println("users code" + code);
-		System.out.println("users username" + username);
-		//String codeStr=Integer.toString(code);
-		//session.setAttribute("code",rstr);
-		request.setAttribute("code",code);
-		return "validate";
-	}
 
-	@RequestMapping("/user**")
-	public String getUserProfile() {
-		//return "user";
-		return "validate";
-	}
 
-	@RequestMapping("/403")
-	public ModelAndView getAccessDenied() {
-		Authentication auth = SecurityContextHolder.getContext()
-				.getAuthentication();
-		String username = "";
-		if (!(auth instanceof AnonymousAuthenticationToken)) {
-			UserDetails userDetail = (UserDetails) auth.getPrincipal();
-			username = userDetail.getUsername();
-		}
+    @RequestMapping(value="/admin**", method = RequestMethod.POST)
+    public String getAdminProfile(@ModelAttribute Users users,HttpServletRequest request) {
+        System.out.print("-----users--------"+users.getCaptchaCode());
+        String code = users.getCaptchaCode();
+        String username = users.getUsername();
+        System.out.println("users code" + code);
+        System.out.println("users username" + username);
+        request.setAttribute("code",code);
+        return "validate";
+    }
 
-		return new ModelAndView("403", "username", username);
-	}
-	@RequestMapping("/forgotPassword")
-	public String getForgotPassword(@ModelAttribute Users users) {
-		return "forgotPassword";
-	}
-	@RequestMapping(value = "/forgotPassword", method = RequestMethod.POST)
-	public ModelAndView getpasswordRecovery(@ModelAttribute Users users) {
-		String username= users.getUsername();
-		String password = loginService.providePassword(username);
-		return new ModelAndView("recoverPassword", "password", password);
-			}
+    @RequestMapping("/user**")
+    public String getUserProfile() {
+        //return "user";
+        return "validate";
+    }
+
+    @RequestMapping("/403")
+    public ModelAndView getAccessDenied() {
+        Authentication auth = SecurityContextHolder.getContext()
+                .getAuthentication();
+        String username = "";
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            UserDetails userDetail = (UserDetails) auth.getPrincipal();
+            username = userDetail.getUsername();
+        }
+
+        return new ModelAndView("403", "username", username);
+    }
+    @RequestMapping("/forgotPassword")
+    public String getForgotPassword(@ModelAttribute Users users) {
+        return "forgotPassword";
+    }
+    @RequestMapping(value = "/forgotPassword", method = RequestMethod.POST)
+    public ModelAndView getpasswordRecovery(@ModelAttribute Users users) {
+        String username= users.getUsername();
+        String password = loginService.providePassword(username);
+        return new ModelAndView("recoverPassword", "password", password);
+    }
 }
